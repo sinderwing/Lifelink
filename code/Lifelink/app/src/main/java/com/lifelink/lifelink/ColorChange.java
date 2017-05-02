8b652419168a3f4e3ba0a13d85484e1c2c1f5d25
John Nilsson <chrysaries@hotmail.com>
2017-05-02 18:27:04 +0200

    Buttons (#15)
    * Functional buttons for MainActivity; placement weird
    
    * Deleting test projects added by accident
    
    * First stable build

--- code/Lifelink/app/src/main/java/com/lifelink/lifelink/ColorChange.java
+++ code/Lifelink/app/src/main/java/com/lifelink/lifelink/ColorChange.java
@@ -1,11 +1,8 @@
 package com.lifelink.lifelink;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class ColorChange extends AppCompatActivity {
 
@@ -13,17 +10,5 @@ public class ColorChange extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_change);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
--- code/Lifelink/app/src/main/java/com/lifelink/lifelink/NameChange.java
+++ code/Lifelink/app/src/main/java/com/lifelink/lifelink/NameChange.java
@@ -1,11 +1,7 @@
 package com.lifelink.lifelink;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class NameChange extends AppCompatActivity {
 
@@ -13,17 +9,5 @@ public class NameChange extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_change);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
