5c1b3648d3aad21949fd2c2274a06c3766556cd7
John Nilsson <chrysaries@hotmail.com>
2017-05-02 21:07:46 +0200

    Working life counter for singleplayer

--- code/Lifelink/app/src/main/java/com/lifelink/lifelink/Ingame.java
+++ code/Lifelink/app/src/main/java/com/lifelink/lifelink/Ingame.java
@@ -1,9 +1,10 @@
 package com.lifelink.lifelink;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,34 +15,57 @@ public class Ingame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingame);
-        TextView textView = (TextView) findViewById(R.id.lifeTotalDisplay);
-        textView.setText(lifeTotal);
+        final TextView display = (TextView) findViewById(R.id.lifeTotalDisplay);
+        display.setText(String.valueOf(this.getLifeTotal()));
 
         //Buttons
         Button plus1 = (Button) findViewById(R.id.plus1);
         plus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                increment(1);
+                increment(1, display);
             }
         });
 
-        Button minus1 = (Button) findViewById(R.id.plus1);
+        Button minus1 = (Button) findViewById(R.id.minus1);
         minus1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                increment(-1);
+                increment(-1, display);
             }
         });
 
+/**
+        //Display editor
+        TextView.OnEditorActionListener exampleListener = new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView display, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL
+                        && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    setLifeTotal();
+                    updateLifeTotalDisplay(display);
+                    //example_confirm();//match this behavior to your 'Send' (or Confirm) button
+                }
+                return true;
+            }
+        }
+ */
     }
 
     //Life counter implementation
-    public void increment(int amount) {
+    public void increment(int amount, TextView display) {
         lifeTotal += amount;
+        updateLifeTotalDisplay(display);
     }
 
     public void setLifeTotal(int amount) {
         lifeTotal = amount;
+    }
+
+    public int getLifeTotal() {
+        return lifeTotal;
+    }
+
+    public void updateLifeTotalDisplay(TextView display) {
+        display.setText(String.valueOf(this.getLifeTotal()));
     }
 }
