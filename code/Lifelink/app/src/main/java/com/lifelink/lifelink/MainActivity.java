3b8582d9f1386f47c4276d270e364f81d2a8cbd5
John Nilsson <chrysaries@hotmail.com>
2017-05-05 17:10:54 +0200

    Adding launch icon (round and non-round)

--- code/Lifelink/app/src/main/java/com/lifelink/lifelink/MainActivity.java
+++ code/Lifelink/app/src/main/java/com/lifelink/lifelink/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences playerProfile = getSharedPreferences("playerProfile", Context.MODE_PRIVATE);
         nameInput.setText(playerProfile.getString("name", "empty"));
-        colorInput.setText(playerProfile.getString("color", "ffffffff")); //color white if nothing else
+        colorInput.setText(playerProfile.getString("color", "ffffff")); //color white if nothing else
     }
 
     /**
