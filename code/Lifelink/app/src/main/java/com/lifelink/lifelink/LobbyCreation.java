94edf8195354e6e6ece14425b366fef5d811da23
John Nilsson <chrysaries@hotmail.com>
2017-05-17 17:19:02 +0200

    Finishing LobbyCreation activity

--- code/Lifelink/app/src/main/java/com/lifelink/lifelink/LobbyCreation.java
+++ code/Lifelink/app/src/main/java/com/lifelink/lifelink/LobbyCreation.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.ToggleButton;
 import android.widget.TextView;
@@ -63,8 +64,13 @@ public class LobbyCreation extends AppCompatActivity {
         });
 
          // SeekBar and TextView to set and show the starting time value.
-        SeekBar setTimeValue = (SeekBar) findViewById(R.id.setTimeValue);
+        final SeekBar setTimeValue = (SeekBar) findViewById(R.id.setTimeValue);
+        final int offsetTime = 20; //minimum time limit
+        int preferredTime = Integer.parseInt(playerProfile.getString("preferredTime", "180"));
+        setTimeValue.setProgress(preferredTime-offsetTime);
+        setTimeValue.setMax(6*60 - offsetTime); //Six minutes
         final TextView currentTimeValue = (TextView) findViewById(R.id.currentTimeValue);
+        currentTimeValue.setText("" + (setTimeValue.getProgress() + offsetTime));
         setTimeValue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
             /**
@@ -77,6 +83,9 @@ public class LobbyCreation extends AppCompatActivity {
              */
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress += offsetTime;
+                progress = progress / 10;
+                progress = progress * 10; //rounds off to lower int, e.g. 18 --> 10
                 currentTimeValue.setText(String.valueOf(progress));
             }
 
@@ -92,8 +101,25 @@ public class LobbyCreation extends AppCompatActivity {
         });
 
          // Toggle time settings on/off.
-         // TODO: Make this work.
         ToggleButton timeOnOff = (ToggleButton) findViewById(R.id.setTimeOnOff);
+        final TextView timeText = (TextView) findViewById(R.id.timeText);
+        timeOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    // Show all time settings
+                    currentTimeValue.setVisibility(View.VISIBLE);
+                    setTimeValue.setVisibility(View.VISIBLE);
+                    timeText.setVisibility(View.VISIBLE);
+                } else {
+                    // The toggle is disabled
+                    // Hide all time settings
+                    currentTimeValue.setVisibility(View.INVISIBLE);
+                    setTimeValue.setVisibility(View.INVISIBLE);
+                    timeText.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
 
         // Button to go to the Lobby.
@@ -114,7 +140,5 @@ public class LobbyCreation extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
-
 }
